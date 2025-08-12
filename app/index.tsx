b781affa05488313e3cbe5d090871835e153ebe8
Hayat-07 <hayat1607207@gmail.com>
2025-08-12 08:37:45 +0600

    Initial_comit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-slate-900 "
+    >
+      <Text className="text-white font-bold text-2xl text-wrap">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
